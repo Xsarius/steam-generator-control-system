22209bb5463915a52e02d804a45ec8ee3f9804b9
Xsarius <kuba.jaloszynski@gmail.com>
2023-02-02 21:24:48 +0100

    setup script error update

--- setup_sg.sh
+++ setup_sg.sh
@@ -3,23 +3,18 @@
 echo "Setup started"
 sudo apt update
 
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
- echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+xenial \
+stable"
 
 sudo apt update
 
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo docker compose version
-
-git clone https://github.com/Xsarius/steam-generator-control-system.git webapp
