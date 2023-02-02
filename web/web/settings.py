d406b005d8b576ee6079691925c00965492fe7dc
Xsarius <kuba.jaloszynski@gmail.com>
2023-02-02 10:36:13 +0100

    Added .env template

--- web/web/settings.py
+++ web/web/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = os.environ.get("DEBUG", default=False)
 
 ALLOWED_HOSTS = str(os.environ.get("ALLOWED_HOSTS")).split(" ")
 
