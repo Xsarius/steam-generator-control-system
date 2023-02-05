d0cd81b03978ad30d5acb242c0b5f20b8a71e650
Xsarius <kuba.jaloszynski@gmail.com>
2023-02-05 21:51:49 +0100

    rm timestamp from db added default values

--- web/webcontrol/models.py
+++ web/webcontrol/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
 class SteamGenerator(models.Model):
-    time_stamp = models.DateTimeField(auto_now_add=True) # Adds automaticaly timestamp to each measurement
-    water_temp = models.FloatField()
-    steam_temp_1 = models.FloatField()
-    steam_temp_2 = models.FloatField()
+    water_temp = models.FloatField(default=0)
+    steam_temp_1 = models.FloatField(default=0)
+    steam_temp_2 = models.FloatField(default=0)
     pressure = models.FloatField(null=True)
-    heater_water1_power = models.FloatField()
-    heater_water2_power = models.FloatField()
-    heater_water3_power = models.FloatField()
-    heater_steam_power = models.FloatField()
-    valve = models.CharField(max_length=10)
-    measurement_num = models.IntegerField()
+    heater_water1_power = models.FloatField(default=0)
+    heater_water2_power = models.FloatField(default=0)
+    heater_water3_power = models.FloatField(default=0)
+    heater_steam_power = models.FloatField(default=0)
+    valve = models.CharField(max_length=10, default='closed')
+    measurement_num = models.IntegerField(default=0)
