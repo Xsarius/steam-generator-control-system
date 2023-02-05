09225091ea6e6a70e186f9712cebc5097870be09
Xsarius <kuba.jaloszynski@gmail.com>
2023-02-05 17:54:24 +0100

    setup script udpate

--- setup_sg.sh
+++ setup_sg.sh
@@ -3,18 +3,7 @@
 echo "Setup started"
 sudo apt update
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo apt-key fingerprint 0EBFCD88
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-xenial \
-stable"
-
-sudo apt update
-
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+curl -fsSL https://get.docker.com | sh
+sudo apt-get install docker-compose-plugin
 
 sudo docker compose version
