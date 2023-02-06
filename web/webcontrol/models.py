ec9963bba57b69cf5dd01f5adc70833fec2785d1
Xsarius <kuba.jaloszynski@gmail.com>
2023-02-06 18:30:56 +0100

    Update model.py

--- web/webcontrol/models.py
+++ web/webcontrol/models.py
@@ -2,14 +2,13 @@ from django.db import models
 
 # Create your models here.
 class SteamGenerator(models.Model):
-    time_stamp = models.DateTimeField(auto_now_add=True) # Adds automaticaly timestamp to each measurement
-    water_temp = models.FloatField()
-    steam_temp_1 = models.FloatField()
-    steam_temp_2 = models.FloatField()
-    pressure = models.FloatField(null=True)
-    heater_water1_power = models.FloatField()
-    heater_water2_power = models.FloatField()
-    heater_water3_power = models.FloatField()
-    heater_steam_power = models.FloatField()
+    water_temp = models.FloatField(default=0)
+    steam_temp_1 = models.FloatField(default=0)
+    steam_temp_2 = models.FloatField(default=0)
+    pressure = models.FloatField(default=0, null=True)
+    heater_water1_power = models.FloatField(default=0)
+    heater_water2_power = models.FloatField(default=0)
+    heater_water3_power = models.FloatField(default=0)
+    heater_steam_power = models.FloatField(default=0)
     valve = models.CharField(max_length=10)
-    measurement_num = models.IntegerField()
+    measurement_num = models.IntegerField(default=0)
