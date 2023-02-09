53445f10067e4cfc0e1c371a59ede7e0eaa385e4
Xsarius <kuba.jaloszynski@gmail.com>
2023-02-09 21:29:32 +0100

    update

--- web/webcontrol/views.py
+++ web/webcontrol/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse, HttpResponseBadRequest
 from webcontrol.tasks import controller
+
 class Index(View):
 
     def get(self, request, *args, **kwargs):
